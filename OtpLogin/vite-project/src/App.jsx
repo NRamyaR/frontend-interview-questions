812af5cadd43480f692daf53ab4d507b82b975c0
Ramya N R <ramyanr@RamyaYadav.local>
2024-01-17 13:02:16 +0530

    otp form, input validation & otp keypad validation

---
+++ OtpLogin/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PhoneOtpForm from "./Components/PhoneOtpForm";
+function App() {
+  return (
+    <div className="App">
+      <h1>Login with Phone</h1>
+      <PhoneOtpForm />
+    </div>
+  );
+}
+
+export default App;
